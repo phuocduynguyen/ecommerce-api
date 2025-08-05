fb8c9e169de402c7cbfefa990c10b9c559f83787
PhuocDuyNguyen <phuocnt150799@gmail.com>
2025-08-05 16:10:18 +0700

    add authV2

--- src/auth/authUtils.ts
+++ src/auth/authUtils.ts
@@ -9,7 +9,8 @@ import { ObjectId } from 'mongodb'
 const HEADER = {
   API_KEY: 'x-api-key',
   AUTHORIZATION: 'authorization',
-  CLIENT_ID: 'x-client-id'
+  CLIENT_ID: 'x-client-id',
+  REFRESH_TOKEN: 'refresh-token'
 }
 
 interface CreateTokenPairParams {
@@ -41,6 +42,66 @@ const createTokenPair = async ({ payload, privateKey, publicKey }: CreateTokenPa
     throw new Error('Failed to create token pair')
   }
 }
+
+const authenticationV2 = asyncHandler(async (req: any, res: Response, next: NextFunction) => {
+  //step 1: check userId missing
+  const userId = req.headers[HEADER.CLIENT_ID]
+  if (!userId) {
+    throw createHttpError(401, 'Invalid request')
+  }
+  //step 2: get accessToken
+  const keyStore: any = await KeyTokenService.findByUserId(userId as ObjectId)
+  if (!keyStore) {
+    throw createHttpError(404, 'Key store not found')
+  }
+
+  //Check refresh token
+  if (req.headers[HEADER.REFRESH_TOKEN]) {
+    const refreshToken = req.headers[HEADER.REFRESH_TOKEN]
+    if (!refreshToken) {
+      throw createHttpError(401, 'Invalid request')
+    }
+    try {
+      const decodedRefresh = jwt.verify(refreshToken, keyStore.privateKey)
+      if (typeof decodedRefresh === 'object' && 'userId' in decodedRefresh) {
+        if (decodedRefresh.userId !== userId) {
+          throw createHttpError(401, 'Invalid user ID')
+        }
+      } else {
+        throw createHttpError(401, 'Invalid token payload')
+      }
+      req.keyStore = keyStore
+      req.user = decodedRefresh
+      req.refreshToken = refreshToken
+      return next()
+    } catch (error) {
+      throw error instanceof jwt.JsonWebTokenError
+    }
+  }
+  //step 3: verify accessToken
+  const accessToken = req.headers[HEADER.AUTHORIZATION]
+  if (!accessToken) {
+    throw createHttpError(401, 'Invalid request')
+  }
+  try {
+    const decodedUser = jwt.verify(accessToken, keyStore.publicKey)
+    if (typeof decodedUser === 'object' && 'userId' in decodedUser) {
+      if (decodedUser.userId !== userId) {
+        throw createHttpError(401, 'Invalid user ID')
+      }
+    } else {
+      throw createHttpError(401, 'Invalid token payload')
+    }
+    req.keyStore = keyStore
+    req.user = decodedUser
+    return next()
+  } catch (error) {
+    throw error instanceof jwt.JsonWebTokenError
+  }
+  //step 4: check user in db
+  //step 5: check keyStore with this userId
+  //step 6: OK all => Return next()
+})
 
 const authentication = asyncHandler(async (req: any, res: Response, next: NextFunction) => {
   //step 1: check userId missing
@@ -78,4 +139,4 @@ const authentication = asyncHandler(async (req: any, res: Response, next: NextFu
   //step 6: OK all => Return next()
 })
 
-export { createTokenPair, authentication }
+export { createTokenPair, authentication, authenticationV2 }
